edda765fa44dabe035833ec108991bc132386f12
AlexTT <aletz1305@gmail.com>
2020-11-15 13:59:13 -0600

    Primera conexion

--- app/myapp.py
+++ app/myapp.py
@@ -1,4 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/alex/Projects/Flask/TodoApp/app/database/tasks.db'
+db = SQLAlchemy(app)
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255))
+    done = db.Column(db.Boolean)
+
+@app.route('/')
+def saludo():
+    return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return "Administrar Contenido"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
