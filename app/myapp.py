4e11db160b3d3538518185aadd2c5b945971fb4c
AlexTT <aletz1305@gmail.com>
2020-11-15 17:52:16 -0600

    Primera Version

--- app/myapp.py
+++ app/myapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,13 +11,40 @@ class Tasks(db.Model):
     done = db.Column(db.Boolean)
 
 @app.route('/')
-def saludo():
-    return render_template('index.html')
+def home():
+    tasks = Tasks.query.all()
+    return render_template('index.html', tasks = tasks)
+
+@app.route('/add', methods=['POST'])
+def create_task():
+    task = Tasks(content = request.form['content'], done = False)
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/done/<id>')
+def done(id):
+    task = Tasks.query.filter_by(id=int(id)).first()
+    task.done = True
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/undone/<id>')
+def undone(id):
+    task = Tasks.query.filter_by(id=int(id)).first()
+    task.done = False
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Tasks.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route('/admin')
 def admin():
     return "Administrar Contenido"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
